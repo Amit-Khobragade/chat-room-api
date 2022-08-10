cb4195ba3cf2f3ffb30e66249745934f0bc35a80
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-10 16:20:50 +0530

    Added web sockets for instant messaging messages are a way of doing various actions such as creating a room , joining a room, messaging etc.

---
+++ index.js
@@ -0,0 +1,49 @@
+const app = require("express")();
+require("dotenv").config();
+const http = require("http");
+const ws = require("ws");
+const {
+  createRoom,
+  createUser,
+  joinRoom,
+  quitRoom,
+  sendMessage,
+} = require("./src/rooms");
+
+const server = http.createServer(app);
+const wss = new ws.WebSocketServer({ server });
+
+// # --- Connection
+wss.on("connection", function (socket, message) {
+  // # --- error
+  socket.on("error", socket.close);
+
+  const user = createUser(socket, message.headers["user-name"]);
+
+  // # --- Message
+  socket.on("message", async function (message) {
+    let data = null;
+
+    try {
+      data = JSON.parse(message);
+    } catch (err) {
+      socket.emit("error", 4001, "Bad Request");
+      return;
+    }
+
+    if (data.roomId) {
+      if (data.message) sendMessage(user, data);
+      else if (data.isQuitting) quitRoom(user, data);
+    }
+
+    if (data.roomName && data.pswd) {
+      data.isCreation && (await createRoom(data));
+      joinRoom(user, data);
+    }
+  });
+
+  // # --- close
+  socket.on("close", (...reason) => console.log(reason.toString()));
+});
+
+server.listen(process.env.PORT);
